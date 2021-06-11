1408532c9c2f26609ab3c317e784cebc22872f50
mykhailo.shakhov <mykhailo.shakhov@eleks.com>
2021-06-11 20:01:03 +0300

    Modified DataBase Inserting

--- spark_processing.py
+++ spark_processing.py
@@ -6,22 +6,30 @@ import json
 import psycopg2
 
 
-def add_to_db(message: pyspark.rdd.RDD, hostname=1,username=1, password=1, database=1):
-    # print(type(message))
+def add_to_db(message: pyspark.rdd.RDD, hostname="34.94.182.239",
+              username='pivotal', password='ZSf5rUAs0EEwr',
+              database='pages'):
+
     # message.foreach(lambda x: print(x))
     new_mess = message.take(100000000)
-
+    print("Test!")
     conn = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
-
+    print("Test accepted!")
     cursor = conn.cursor()
 
     for mess in new_mess:
-        cursor.execute("insert into Page (page_id, page_name, created_at, user_id, domain_name) "
-                       "values (%s, %s, %s, %s, %s, %s) ",
-                       [mess["page_id"], mess["page_title"], mess["rev_timestamp"],
-                        mess["user_id"], mess["meta"]["domain"]])
+        try:
 
-        conn.commit()
+            data = [mess["page_id"], mess["page_title"], mess["rev_timestamp"],
+                   mess["performer"]["user_id"], mess["meta"]["domain"]]
+            cursor.execute("insert into Pages_Page (page_id, page_name, created_at, user_id, domain_name) "
+                           "values (%s, %s, %s, %s, %s) ",
+                           data)
+
+            conn.commit()
+        except Exception as e:
+            print(e)
+            print(mess)
     conn.close()
     # new_message = message.take(1000000)
 
