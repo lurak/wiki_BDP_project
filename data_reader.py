5063ae74fc22e7f7417c4800ce3b480845f0732c
mykhailo.shakhov <mykhailo.shakhov@eleks.com>
2021-06-13 17:02:54 +0300

    Added Batch Processing, Updated streaming, data reader

--- data_reader.py
+++ data_reader.py
@@ -1,7 +1,5 @@
 from kafka import KafkaProducer
 from sseclient import SSEClient as EventSource
-import requests
-import json
 import sys
 
 
@@ -9,26 +7,13 @@ if __name__ == "__main__":
     producer = KafkaProducer(bootstrap_servers=[sys.argv[1]], value_serializer=lambda x: x.encode('utf-8')) #host:9092
     url = "https://stream.wikimedia.org/v2/stream/page-create"
     lst = []
-    i = 20
     for event in EventSource(url):
-        if i < 0:
-            break
         if event.event == 'message':
             try:
-                print(event.data)
-                producer.send("topic-1", event.data)
-                # event_data = json.loads(event.data)
-                # lst.append(event_data)
-                # print(event_data)
-                # i -= 1
-            except ValueError:
-                pass
-            # else:
-            #     # filter out events, keep only article edits (mediawiki.recentchange stream)
-            #     if event_data['type'] == 'edit':
-            #         # construct valid json event
-            #         event_to_send = construct_event(event_data, user_types)
-            #
-            #         producer.send('wikipedia-events', value=event_to_send)
-    with open('example.json', 'w', encoding='utf-8') as f:
-        json.dump(lst, f, indent=2)
+                data = '{"id":' + event.id + ',' + \
+                              '"data": ' + event.data + '}'
+                print(data)
+                producer.send("topic-1", data)
+
+            except Exception as e:
+                print(e)
